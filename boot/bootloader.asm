8c5958486f6e773f316f5f0b28a54422064bd89e
prajogotio <ggkgaya@hotmail.com>
2016-12-26 16:39:42 +0800

    Cleaning up bootloader code

--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -7,70 +7,30 @@ jmp loader
 %include "boot/inc/load_from_disk.inc"
 %include "boot/debugger/print_hex.asm"
 
-KERNEL_SECTORS            dd 52
-KERNEL_STARTING_SECTOR    dd 5
-KERNEL_COPY_BASE          dd 0x1000
-
-total_head                db 0
-sectors_per_track         db 0
 
 loader:
   mov si, WelcomeMsg
   call Puts16
+
   mov [BootDrive], dl
 
   ; Place sp at a safe location
   mov sp, 0x9000
 
-
-  ; Get sector_per_track and total_head
-  mov ah, 8
-  mov dl, byte [BootDrive]
-  int 0x13
-
-  mov [total_head], dh      ; number of heads -1
-  and cl, 0x3f
-  mov [sectors_per_track], cl
-
-  ; Load Stage 2
+  ; Load Stage 2 at 0x500 RAM
   mov ax, 0x50
   mov es, ax
   mov bx, 0
-  mov al, 3
-  mov cl, 2
+  mov al, 3           ; Second stage has size 3 x 512 bytes
+  mov cl, 2           ; Second stage starts at the second sector.
   mov dl, [BootDrive]
   call LoadFromDisk
-
-
-  mov si, WelcomeMsg
-  call Puts16
-
-  ; Load Kernel at KERNEL_COPY_BASE
-  mov ax, [KERNEL_COPY_BASE]
-  shr ax, 4
-  mov es, ax
-  mov bx, [KERNEL_COPY_BASE]
-  and bx, 0xf
-  mov al, [KERNEL_SECTORS]          ; load al sectors.
-  mov cl, [KERNEL_STARTING_SECTOR]  ; starting from cl -th sector.
-  mov dl, [BootDrive]
-  call LoadFromDisk
-
-
-  ; Send kernel copy info to over to stage 2
-  mov eax, [KERNEL_COPY_BASE]
-  push eax
-  mov eax, [KERNEL_SECTORS]
-  push eax
-
 
   ; Jump to stage 2
   jmp 0x500
 
-
-BootDrive db 0
-
-WelcomeMsg db "Welcome...", 0
+BootDrive   db  0
+WelcomeMsg  db  "MBR block loaded. Jumping to Stage 2...", 0
 
 times 510 - ($-$$) db 0
-dw 0xaa55
+dw 0xaa55             ; Magic number for MBR
--- boot/enter_protected_mode.asm
+++ boot/enter_protected_mode.asm
@@ -10,29 +10,20 @@ jmp main
 %include "boot/inc/install_gdt.inc"
 %include "boot/debugger/print_hex_32.asm"
 
-LoadingMsg db "Entering Protected Mode", 0x0A, 0x0D, 0x00
-DiskLoadedMsg db "Second stage bootloader loaded!", 0x0A, 0x0D, 0
-WelcomeMsg db "Loading kernel image and enabling paging.", 0x0A, "Setting up...", 0x0A, 0
-KernelLoadedMsg db 0x0A, "Kernel image loaded.", 0x0A, 0
+%define KERNEL_SECTORS        255
+%define KERNEL_START_SECTOR   0x4
+
+LoadingMsg           db "Entering Protected Mode", 0x0A, 0x0D, 0x00
+SecondStageLoadedMsg db "Second stage bootloader loaded!", 0x0A, 0x0D, 0
+WelcomeMsg           db "Loading kernel image and enabling paging.", 0x0A, "Setting up...", 0x0A, 0
+KernelLoadedMsg      db 0x0A, "Kernel image loaded.", 0x0A, 0
 
 main:
-  ; Retrieve kernel_size = 512 * kernel_sector stored in stack
-  ; Deprecated! We will be overwriting on [kernel_size], because
-  ; we will be able to use protected mode ATA PIO disk read, and hence
-  ; can load the kernel directly to 0x00100000
-  pop eax
-  mov ecx, 512
-  mul ecx
-  mov [kernel_size], eax
-
-  pop eax
-  mov [kernel_copy_base], eax
-
-
-  ; Preparing stage 2
-  mov si, DiskLoadedMsg
+  ; Initialization of stage 2.
+  mov si, SecondStageLoadedMsg
   call Puts16
   
+  ; Initialize segment registers and stack pointer.
   cli
   xor ax, ax
   mov ds, ax
@@ -78,13 +69,16 @@ main:
   mov si, LoadingMsg
   call Puts16
 
+  ; Install GDT for protected mode
   call InstallGdt
 
+  ; Enable protected mode
   cli
   mov eax, cr0
   or eax, 1
   mov cr0, eax
 
+  ; Long jump to set cs register to the correct segment selector (0x8).
   jmp 0x8:ProtectedMode
 
 
@@ -102,69 +96,34 @@ ProtectedMode:
   mov ebx, WelcomeMsg
   call Puts32
 
-  ; Enable A20
+  ; Enable A20 to enable 4GB addressing
   mov al, 2
   out 0x92, al
 
-
-  ; Canary: put 0xfaceface at position 0x00100000.
-  mov dword [0x00100000], 0xfaceface
-  mov dword ecx, [0x00100000]
-  call PrintHex32
-
+  ; Install temporary page directory and paging (identity mapping and 3GB virtual to 0x00100000 mapping)
   call EnablePaging
 
-  ; Test canary: if paging works, 0xc0000000 should contain 0xfaceface
-  mov dword ecx, [0xc0000000]
-  call PrintHex32
-
-  ; Load kernel to 0xc0000000
+  ; Load kernel to 0xc0000000 virtual (which is 0x00100000 physical)
   ; Load up to 255 sectors first
   ; Kernel image starts at 5-th sector on disk.
-  ; Hence LBA = 4
-  mov dword [kernel_size], 255 * 512
-  mov cl, 255
-  mov eax, 0x4
+  ; Hence LBA = 4 (LBA stands for logical block addressing, each block is 512 bytes)
+  mov cl, KERNEL_SECTORS
+  mov eax, KERNEL_START_SECTOR
   mov edi, 0xc0000000
   call AtaPioLbaRead
 
   mov ebx, KernelLoadedMsg
   call Puts32
 
-  mov dword ecx, [0xc0005a64]
-  call PrintHex32
+  ; Update kernel_size information
+  mov dword [kernel_size], KERNEL_SECTORS * 512
 
-
-;;  ; Deprecated! Since we are loading kernel directly to 0xc0000000 from disk,
-;;  ; We don't need to copy the kernel image from 0x1000 anymore.
-;;  ; Move Kernel to KERNEL_PROTECTED_BASE
-;;  ; from [kernel_copy_base]
-;;  mov ecx, 0
-;;.CopyKernelWhileLoop:
-;;
-;;  mov ebx, [kernel_copy_base]
-;;  add ebx, ecx
-;;  mov ebx, [ebx]
-;;  mov [KERNEL_PROTECTED_BASE + ecx], ebx
-;;
-;;  inc ecx
-;;  cmp ecx, [kernel_size]
-;;  je .CopyKernelDone
-;;  jmp .CopyKernelWhileLoop
-;;
-;;.CopyKernelDone:
-
-straight_to_kernel:
   ; Jump to Kernel
-  ; Set address of memory_info to ebx
+  ; Set address of memory info table to ebx
   mov ebx, kernel_memory_table
 
   jmp 0x8:KERNEL_PROTECTED_BASE
 
-
-Stop:
-  cli
-  hlt
 
 %include "boot/inc/Puts32.inc"
 %include "boot/inc/memory_map.inc"
@@ -173,7 +132,6 @@ Stop:
 
 kernel_copy_base
   dd 0
-
 kernel_memory_table:
 kernel_size:
   dd 0
