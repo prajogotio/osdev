255ffc8ddcef09b275445153c631f2ab901f82a2
prajogotio <ggkgaya@hotmail.com>
2016-12-26 16:22:47 +0800

    Implement direct kernel image loading via ata pio at protected mode entry stage, removing restriction of < 30KB kernel image

--- boot/enter_protected_mode.asm
+++ boot/enter_protected_mode.asm
@@ -10,12 +10,16 @@ jmp main
 %include "boot/inc/install_gdt.inc"
 %include "boot/debugger/print_hex_32.asm"
 
-LoadingMsg db "Preparing to load operating system...", 0x0A, 0x0D, 0x00
-DiskLoadedMsg db "Disk Loaded!", 0x0A, 0x0D, 0
-WelcomeMsg db "    Welcome to TIO OS!!!!!", 0x0A, "tixxo os tio os tio os yoo", 0
+LoadingMsg db "Entering Protected Mode", 0x0A, 0x0D, 0x00
+DiskLoadedMsg db "Second stage bootloader loaded!", 0x0A, 0x0D, 0
+WelcomeMsg db "Loading kernel image and enabling paging.", 0x0A, "Setting up...", 0x0A, 0
+KernelLoadedMsg db 0x0A, "Kernel image loaded.", 0x0A, 0
 
 main:
   ; Retrieve kernel_size = 512 * kernel_sector stored in stack
+  ; Deprecated! We will be overwriting on [kernel_size], because
+  ; we will be able to use protected mode ATA PIO disk read, and hence
+  ; can load the kernel directly to 0x00100000
   pop eax
   mov ecx, 512
   mul ecx
@@ -70,7 +74,6 @@ main:
 
   call GetMemoryMap
 
-
   ; Going to protected mode
   mov si, LoadingMsg
   call Puts16
@@ -94,13 +97,15 @@ ProtectedMode:
   mov es, ax
   mov esp, 0x90000
   
+  call ClearScreen32
+
+  mov ebx, WelcomeMsg
+  call Puts32
+
   ; Enable A20
   mov al, 2
   out 0x92, al
 
-  call ClearScreen32
-  mov ebx, WelcomeMsg
-  call Puts32
 
   ; Canary: put 0xfaceface at position 0x00100000.
   mov dword [0x00100000], 0xfaceface
@@ -113,25 +118,43 @@ ProtectedMode:
   mov dword ecx, [0xc0000000]
   call PrintHex32
 
+  ; Load kernel to 0xc0000000
+  ; Load up to 255 sectors first
+  ; Kernel image starts at 5-th sector on disk.
+  ; Hence LBA = 4
+  mov dword [kernel_size], 255 * 512
+  mov cl, 255
+  mov eax, 0x4
+  mov edi, 0xc0000000
+  call AtaPioLbaRead
 
-  ; Move Kernel to KERNEL_PROTECTED_BASE
-  ; from [kernel_copy_base]
-  mov ecx, 0
+  mov ebx, KernelLoadedMsg
+  call Puts32
 
-.CopyKernelWhileLoop:
+  mov dword ecx, [0xc0005a64]
+  call PrintHex32
 
-  mov ebx, [kernel_copy_base]
-  add ebx, ecx
-  mov ebx, [ebx]
-  mov [KERNEL_PROTECTED_BASE + ecx], ebx
 
-  inc ecx
-  cmp ecx, [kernel_size]
-  je .CopyKernelDone
-  jmp .CopyKernelWhileLoop
+;;  ; Deprecated! Since we are loading kernel directly to 0xc0000000 from disk,
+;;  ; We don't need to copy the kernel image from 0x1000 anymore.
+;;  ; Move Kernel to KERNEL_PROTECTED_BASE
+;;  ; from [kernel_copy_base]
+;;  mov ecx, 0
+;;.CopyKernelWhileLoop:
+;;
+;;  mov ebx, [kernel_copy_base]
+;;  add ebx, ecx
+;;  mov ebx, [ebx]
+;;  mov [KERNEL_PROTECTED_BASE + ecx], ebx
+;;
+;;  inc ecx
+;;  cmp ecx, [kernel_size]
+;;  je .CopyKernelDone
+;;  jmp .CopyKernelWhileLoop
+;;
+;;.CopyKernelDone:
 
-.CopyKernelDone:
-
+straight_to_kernel:
   ; Jump to Kernel
   ; Set address of memory_info to ebx
   mov ebx, kernel_memory_table
@@ -146,6 +169,7 @@ Stop:
 %include "boot/inc/Puts32.inc"
 %include "boot/inc/memory_map.inc"
 %include "boot/inc/paging.inc"
+%include "boot/inc/read_from_disk_protected_mode.inc"
 
 kernel_copy_base
   dd 0
---
+++ kernel/core/disk_allocation.h
@@ -0,0 +1,7 @@
+#ifndef __TIO_OS_DISK_ALLOCATION_H__
+#define __TIO_OS_DISK_ALLOCATION_H__
+
+// Approach used: bitmap allocation table
+// Locate the
+
+#endif  //__TIO_OS_DISK_ALLOCATION_H__
